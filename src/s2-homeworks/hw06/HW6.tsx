a399ce9068157426d1c5d94ae8d52349617d7f9c
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-10-04 11:43:51 +0200

    HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -15,11 +15,11 @@ const HW6 = () => {
     const [value, setValue] = useState<string>('')
 
     const save = () => {
-        saveState<string>('hw6-editable-span-value', value)
+        saveState<string>('hw6-editable-span-value', value);
     }
     const restore = () => {
         // делают студенты
-
+        setValue(restoreState<string>('hw6-editable-span-value', value));
     }
 
     return (
