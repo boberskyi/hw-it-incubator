409e023c768fc01d2cc67733a45768cf43348fba
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-11-13 20:09:57 +0100

    HW 11

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import {BrowserRouter, HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import { Layout } from './layout/Layout'
 import Pages from './Pages'
-
-/*
-* 1 - в файле Pages.tsx дописать роуты на все страницы
-* 2 - в файле Sidebar.tsx дописать className так чтоб вешался класс s.active когда мы уже на соответствующей странице
-* 3 - застилизовать хэдэр и сайдбар в соответствии с дизайном
-* */
 
 function HW5() {
     return (
         <BrowserRouter>
-            {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
             <Layout>
                 <Pages />
             </Layout>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {Slider, SliderProps, SliderThumb} from '@mui/material'
+import { styled } from '@mui/material/styles';
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
+        <MyCustomSlider {...props} />
     )
 }
+
+const MyCustomSlider = styled(Slider)(() => ({
+    width: 147,
+    color: '#0C2;',
+    height: 4,
+    '& .MuiSlider-thumb': {
+        height: 18,
+        width: 18,
+        backgroundColor: '#fff',
+        border: '1px solid currentColor',
+        '&:hover': {
+            boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
+        },
+        '&:before': {
+            height: 6,
+            width: 6,
+            filter: 'drop-shadow(0px 0px 2px rgba(29, 33, 38, 0.10)) drop-shadow(0px 0px 40px rgba(29, 33, 38, 0.13))',
+            backgroundColor: 'currentColor',
+        },
+    },
+    '& .MuiSlider-track': {
+        height: 4,
+    },
+    '& .MuiSlider-rail': {
+        color: '#8B8B8B',
+        height: 4,
+    },
+}));
 
 export default SuperRange
